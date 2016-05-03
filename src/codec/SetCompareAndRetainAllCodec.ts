b54dd855bec8d160dbfa68650e2d7579ec2b5f50
Mustafa Iman <mustafaiman@gmail.com>
2016-05-03 11:25:55 +0300

    adds set codecs

---
+++ src/codec/SetCompareAndRetainAllCodec.ts
@@ -0,0 +1,58 @@
+/* tslint:disable */
+import ClientMessage = require('../ClientMessage');
+import {BitsUtil} from '../BitsUtil';
+import Address = require('../Address');
+import {AddressCodec} from './AddressCodec';
+import {MemberCodec} from './MemberCodec';
+import {Data} from '../serialization/Data';
+import {EntryViewCodec} from './EntryViewCodec';
+import DistributedObjectInfoCodec = require('./DistributedObjectInfoCodec');
+import {SetMessageType} from './SetMessageType';
+
+var REQUEST_TYPE = SetMessageType.SET_COMPAREANDRETAINALL;
+var RESPONSE_TYPE = 101;
+var RETRYABLE = false;
+
+
+export class SetCompareAndRetainAllCodec{
+
+
+
+static calculateSize(name : string  , values : any ){
+// Calculates the request payload size
+var dataSize : number = 0;
+            dataSize += BitsUtil.calculateSizeString(name);
+        dataSize += BitsUtil.INT_SIZE_IN_BYTES;
+
+        values.forEach((valuesItem : any) => {
+            dataSize += BitsUtil.calculateSizeData(valuesItem);
+        });
+return dataSize;
+}
+
+static encodeRequest(name : string, values : any){
+// Encode request into clientMessage
+var clientMessage = ClientMessage.newClientMessage(this.calculateSize(name, values));
+clientMessage.setMessageType(REQUEST_TYPE);
+clientMessage.setRetryable(RETRYABLE);
+    clientMessage.appendString(name);
+    clientMessage.appendInt32(values.length);
+
+    values.forEach((valuesItem : any) => {
+    clientMessage.appendData(valuesItem);
+    });
+
+clientMessage.updateFrameLength();
+return clientMessage;
+}
+
+static decodeResponse(clientMessage : ClientMessage,  toObjectFunction: (data: Data) => any = null){
+// Decode response from client message
+var parameters :any = { 'response' : null  };
+                    parameters['response'] = clientMessage.readBoolean();
+return parameters;
+
+}
+
+
+}
---
+++ src/codec/SetMessageType.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+export class SetMessageType {
+static SET_SIZE = 0x0601;
+static SET_CONTAINS = 0x0602;
+static SET_CONTAINSALL = 0x0603;
+static SET_ADD = 0x0604;
+static SET_REMOVE = 0x0605;
+static SET_ADDALL = 0x0606;
+static SET_COMPAREANDREMOVEALL = 0x0607;
+static SET_COMPAREANDRETAINALL = 0x0608;
+static SET_CLEAR = 0x0609;
+static SET_GETALL = 0x060a;
+static SET_ADDLISTENER = 0x060b;
+static SET_REMOVELISTENER = 0x060c;
+static SET_ISEMPTY = 0x060d;
+}
