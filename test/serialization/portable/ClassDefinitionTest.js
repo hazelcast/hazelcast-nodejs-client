1172554d9fe6767e50afc36c49df8fe3920682a7
Aykut Bozkurt <aykut@Aykuts-MacBook-Pro.local>
2016-06-22 12:25:22 +0300

    method tests assembled into a suite test

--- test/serialization/portable/ClassDefinitionTest.js
+++ test/serialization/portable/ClassDefinitionTest.js
@@ -4,101 +4,71 @@ var CD = require('../../../lib/serialization/portable/ClassDefinition').ClassDef
 var FD = require('../../../lib/serialization/portable/ClassDefinition').FieldDefinition;
 var FT = require('../../../lib/serialization/portable/ClassDefinition').FieldDefinition;
 
-describe('Test.getFieldType()', function(){
+describe('ClassDefinitionTest', function(){
     var a = new CD(1,2,3);
 
-    it('firstField should be type of FT.INT.', function(){
+    it('getFieldType():firstField should be type of FT.INT.', function(){
         a.addFieldDefinition(new FD(0,'firstField',FT.INT,10,10));
         var type = a.getFieldType('firstField');
         expect(type).equal(FT.INT);
     });
 
-    it('secondField should be type of FT.STRING.', function(){
+    it('getFieldType():secondField should be type of FT.STRING.', function(){
         a.addFieldDefinition(new FD(1,'secondField',FT.CHAR_ARRAY,11,11));
         var type = a.getFieldType('secondField');
         expect(type).equal(FT.CHAR_ARRAY);
     });
 
-    it('thirdField should be null because we will not add it. (It does not exist.)', function(){
+    it('getFieldType():thirdField should be null because we will not add it. (It does not exist.)', function(){
         expect(a.getFieldType.bind(a,'thirdField')).to.throw(Error);
     });
 
-});
 
-describe('Test.hasField()', function(){
-    var a;
-
-    beforeEach(function(){
-        a = new CD(1,2,3);
-    });
-
-    it('field was added so it should be return true.', function(){
-        a.addFieldDefinition(new FD(0,'aField',FT.INT,10,10));
-        //var res = a.hasField('aField');
+    it('hasField():field was added so it should be return true.', function(){
+        a.addFieldDefinition(new FD(2,'aField',FT.INT,10,10));
         expect(a.hasField('aField')).to.be.true;
     });
 
-    it('field was not added so it should be return false.', function(){
-        var res = a.hasField('aField');
+    it('hasField():field was not added so it should be return false.', function(){
+        var res = a.hasField('anotherField');
         expect(res).to.be.false;
     });
 
-});
-
-
-
-describe('Test.getFieldById()', function(){
-    var a = new CD(1,2,3);
-
-    before(function(){
-        a.addFieldDefinition(new FD(0,'firstField',FT.INT,10,10));
-    });
-
-    it('When index is not an integer it should throw an range exception.', function(){
+    it('getFieldById():When index is not an integer it should throw an range exception.', function(){
         expect(a.getFieldById.bind(a, 0.3)).to.throw(Error);
     });
 
-    it('When index is negative it should throw an range exception.', function(){
+    it('getFieldById():When index is negative it should throw an range exception.', function(){
         expect(a.getFieldById.bind(a, -2)).to.throw(Error);
     });
 
-    it('When a field with given index is found.', function(){
+    it('getFieldById():When a field with given index is found.', function(){
         expect(a.getFieldById.bind(a, 0)).to.not.throw(Error);
     });
 
-    it('When index is more than or equal to total number of field it should throw an range exception.', function(){
-        expect(a.getFieldById.bind(a, 1)).to.throw(Error);
+    it('getFieldById():When index is more than or equal to total number of fields it should throw an range exception.', function(){
+        expect(a.getFieldById.bind(a, 3)).to.throw(Error);
     });
 
-    /*it('When there is no such field', function(){
-       a.addFieldDefinition(new FD(2,'firstField',FT.INT,10,10));
-        expect(a.getFieldById.bind(a,1)).not.throw(Error);
-    });*/
-
-});
-
-describe('Test.equals()', function(){
-
-    it('When parameter passed is not a ClassDefinitionObject.', function(){
-        var a = new CD();
-        var b = new FD();
-        var res = a.equals(b);
+    it('equals():When parameter passed is not a ClassDefinitionObject.', function(){
+        var a1 = new CD();
+        var b1 = new FD();
+        var res = a1.equals(b1);
         expect(res).to.equal(false);
     });
 
-    it('When one of the fields (or more) is not matched with the parameter.', function(){
-        var a = new CD(1,3,4);
-        var b = new CD(1,3,6);
-        var res = a.equals(b);
+    it('equals():When one of the fields (or more) is not matched with the parameter.', function(){
+        var a1 = new CD(1,3,4);
+        var b1 = new CD(1,3,6);
+        var res = a1.equals(b1);
         expect(res).to.equal(false);
     });
 
-    it('When parameter is equal.', function(){
-        var a = new CD(1,3,6);
-        var b = new CD(1,3,6);
-        var res = a.equals(b);
+    it('equals():When parameter is equal.', function(){
+        var a1 = new CD(1,3,6);
+        var b1 = new CD(1,3,6);
+        var res = a1.equals(b1);
         expect(res).to.equal(true);
     });
-
+    
 });
-
