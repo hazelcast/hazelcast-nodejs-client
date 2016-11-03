b76dcecfe207a24b63bc02683d9aa2f5766e5a36
Mustafa Iman <mustafaiman@gmail.com>
2016-11-03 09:46:07 +0100

    ipv4/6 type fix in address

--- src/Address.ts
+++ src/Address.ts
@@ -1,11 +1,13 @@
 import {IdentifiedDataSerializable} from './serialization/Serializable';
 import {DataInput, DataOutput} from './serialization/Data';
 import {ADDRESS_CLASS_ID, CLUSTER_DATA_FACTORY_ID} from './ClusterDataFactory';
+import * as net from 'net';
 
 class Address implements IdentifiedDataSerializable {
 
     host: string;
     port: number;
+    type: number;
 
     static encodeToString(address: Address): string {
         return address.host + ':' + address.port;
@@ -14,19 +16,23 @@ class Address implements IdentifiedDataSerializable {
     constructor(host?: string, port?: number) {
         this.host = host;
         this.port = port;
+        if (net.isIPv6(host)) {
+            this.type = 6;
+        } else {
+            this.type = 4;
+        }
     }
 
 
     readData(input: DataInput): any {
         this.port = input.readInt();
-        // IPv4 or IPv6 - not used here
-        input.readByte();
+        this.type = input.readByte();
         this.host = input.readUTF();
     }
 
     writeData(output: DataOutput): void {
         output.writeInt(this.port);
-        output.writeByte(4);
+        output.writeByte(this.type);
         output.writeUTF(this.host);
     }
 
