245ed5b02a7942b1b25ab4a620ea637354cd2f8a
Asım Arslan <arslanasim@gmail.com>
2017-04-11 10:47:11 +0300

    update download script and upgrade server version to 3.8

--- scripts/download-rc.sh
+++ scripts/download-rc.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
-HAZELCAST_VERSION="3.6.5"
-HAZELCAST_RC_VERSION="0.1-SNAPSHOT"
+HZ_VERSION="3.8"
+HAZELCAST_TEST_VERSION=${HZ_VERSION}
+HAZELCAST_VERSION=${HZ_VERSION}
+HAZELCAST_ENTERPRISE_VERSION=${HZ_VERSION}
+HAZELCAST_RC_VERSION="0.2-SNAPSHOT"
 SNAPSHOT_REPO="https://oss.sonatype.org/content/repositories/snapshots"
 RELEASE_REPO="http://repo1.maven.apache.org/maven2"
-ENTERPRISE_REPO="https://repository-hazelcast-l337.forge.cloudbees.com/release/"
+ENTERPRISE_RELEASE_REPO="https://repository-hazelcast-l337.forge.cloudbees.com/release/"
+ENTERPRISE_SNAPSHOT_REPO="https://repository-hazelcast-l337.forge.cloudbees.com/snapshot/"
 
+if [[ ${HZ_VERSION} == *-SNAPSHOT ]]
+then
+	REPO=${SNAPSHOT_REPO}
+	ENTERPRISE_REPO=${ENTERPRISE_SNAPSHOT_REPO}
+else
+	REPO=${RELEASE_REPO}
+	ENTERPRISE_REPO=${ENTERPRISE_RELEASE_REPO}
+fi
 
 mvn dependency:get -DrepoUrl=${SNAPSHOT_REPO} -Dartifact=com.hazelcast:hazelcast-remote-controller:${HAZELCAST_RC_VERSION} -Ddest=hazelcast-remote-controller-${HAZELCAST_RC_VERSION}.jar
-mvn dependency:get -DrepoUrl=${RELEASE_REPO} -Dartifact=com.hazelcast:hazelcast:${HAZELCAST_VERSION} -Ddest=hazelcast-${HAZELCAST_VERSION}.jar
+mvn dependency:get -DrepoUrl=${REPO} -Dartifact=com.hazelcast:hazelcast:${HAZELCAST_VERSION} -Ddest=hazelcast-${HAZELCAST_VERSION}.jar
 
 if [ -n "${HAZELCAST_ENTERPRISE_KEY}" ]; then
-    mvn dependency:get -DrepoUrl=${RELEASE_REPO} -Dartifact=com.hazelcast:hazelcast:${HAZELCAST_VERSION}:jar:tests -Ddest=hazelcast-tests-${HAZELCAST_VERSION}.jar
+    mvn dependency:get -DrepoUrl=${REPO} -Dartifact=com.hazelcast:hazelcast:${HAZELCAST_VERSION}:jar:tests -Ddest=hazelcast-tests-${HAZELCAST_VERSION}.jar
     mvn dependency:get -DrepoUrl=${ENTERPRISE_REPO} -Dartifact=com.hazelcast:hazelcast-enterprise:${HAZELCAST_VERSION} -Ddest=hazelcast-enterprise-${HAZELCAST_VERSION}.jar
     CLASSPATH="hazelcast-remote-controller-${HAZELCAST_RC_VERSION}.jar:hazelcast-enterprise-${HAZELCAST_VERSION}.jar:hazelcast-${HAZELCAST_VERSION}.jar:hazelcast-tests-${HAZELCAST_VERSION}.jar:test/javaclasses"
     echo "Starting Remote Controller ... enterprise ..."
