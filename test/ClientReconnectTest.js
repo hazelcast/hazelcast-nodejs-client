6f1f41ce4a19d54df680c74336f2c4311cae2fc4
Mustafa Iman <mustafaiman@gmail.com>
2017-10-06 09:07:26 -0400

    proxymanager error handling

--- test/ClientReconnectTest.js
+++ test/ClientReconnectTest.js
@@ -15,7 +15,7 @@ describe('Client reconnect', function () {
     });
 
     it('member restarts, while map.put in progress', function () {
-        this.timeout(1200000);
+        this.timeout(4000);
         var member;
         var map;
         return Controller.createCluster(null, null).then(function(cl) {
@@ -45,7 +45,7 @@ describe('Client reconnect', function () {
     });
 
     it('member restarts, while map.put in progress 2', function () {
-        this.timeout(1200000);
+        this.timeout(4000);
         var member;
         var map;
         return Controller.createCluster(null, null).then(function(cl) {
@@ -75,7 +75,7 @@ describe('Client reconnect', function () {
     });
 
     it('create proxy while member is down, member comes back', function () {
-        this.timeout(1200000);
+        this.timeout(4000);
         var member;
         var map;
         return Controller.createCluster(null, null).then(function(cl) {
