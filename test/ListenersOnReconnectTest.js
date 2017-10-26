dbdb3eb64fd68e477372fa0875c04fe9835cdd14
Mustafa Iman <mustafaiman@gmail.com>
2017-10-26 16:44:42 -0400

    fdgdg

--- test/ListenersOnReconnectTest.js
+++ test/ListenersOnReconnectTest.js
@@ -5,7 +5,7 @@ var Config = require('../.').Config;
 var Util = require('./Util');
 var Promise = require('bluebird');
 
-describe.skip('Listeners on reconnect', function () {
+describe('Listeners on reconnect', function () {
 
     this.timeout(120000);
     var client;
