0c39ca80160e6ab85771b16530369fba98e9d80a
Mustafa Iman <mustafaiman@gmail.com>
2017-12-01 15:21:43 -0500

    deletes unused codecs

--- src/codec/TransactionMessageType.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-export class TransactionMessageType {
-    static TRANSACTION_COMMIT = 0x1701;
-    static TRANSACTION_CREATE = 0x1702;
-    static TRANSACTION_ROLLBACK = 0x1703;
-}
