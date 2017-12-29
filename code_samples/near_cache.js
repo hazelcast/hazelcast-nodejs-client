cc02c3b2373f482188b4106e2dd36659e515bc51
Mustafa Iman <mustafaiman@gmail.com>
2017-12-29 17:19:51 +0300

    near cache sample

---
+++ code_samples/near_cache.js
@@ -0,0 +1,41 @@
+var Client = require('../.').Client;
+var Config = require('../.').Config;
+
+var nearCachedMapName = 'nearCachedMap';
+var regularMapName = 'reqularMap';
+var client;
+
+var cfg = new Config.ClientConfig();
+var nearCacheConfig = new Config.NearCacheConfig();
+nearCacheConfig.name = nearCachedMapName;
+nearCacheConfig.invalidateOnChange = true;
+cfg.nearCacheConfigs[nearCachedMapName] = nearCacheConfig;
+
+function do50000Gets(client, mapName) {
+    var timerStart;
+    var timerEnd;
+
+    return client.getMap(mapName).put('item', 'anItem').then(function () {
+        //warm up the cache
+        return client.getMap(mapName).get('item')
+    }).then(function () {
+        timerStart = Date.now();
+        var requests = [];
+        for(var i = 0; i < 50000; i++) {
+            requests.push(client.getMap(mapName).get('item'));
+        }
+        return Promise.all(requests);
+    }).then(function () {
+        timerEnd = Date.now();
+        console.log('Took ' + (timerEnd - timerStart) + ' ms to do 50000 gets on ' + mapName + '.');
+    });
+}
+
+Client.newHazelcastClient(cfg).then(function (cl) {
+    client = cl;
+    return do50000Gets(client, nearCachedMapName);
+}).then(function () {
+    return do50000Gets(client, regularMapName);
+}).then(function () {
+    client.shutdown();
+});
