530fcab39ff0e9fd24da099ac7d6a4eb02ae955e
Mustafa Iman <mustafaiman@gmail.com>
2018-02-28 14:51:10 -0500

    fixes #260

--- test/serialization/PortableSerializersLiveTest.js
+++ test/serialization/PortableSerializersLiveTest.js
@@ -12,6 +12,22 @@ describe('Default serializers with live instance', function() {
     var client;
     var map;
 
+    function getClientConfig() {
+        var cfg = new Config.ClientConfig();
+        cfg.serializationConfig.portableFactories[10] = {
+            create: function(classId) {
+                if (classId === 222) {
+                    return new InnerPortable();
+                } else if (classId === 21) {
+                    return new SimplePortable();
+                } else {
+                    return null;
+                }
+            }
+        };
+        return cfg;
+    }
+
     before(function() {
         return RC.createCluster(null, null).then(function (res) {
             cluster = res;
@@ -19,19 +35,7 @@ describe('Default serializers with live instance', function() {
             return RC.startMember(cluster.id);
         }).then(function (m) {
             member = m;
-            var cfg = new Config.ClientConfig();
-            cfg.serializationConfig.portableFactories[10] = {
-                create: function(classId) {
-                    if (classId === 222) {
-                        return new InnerPortable();
-                    } else if (classId === 21) {
-                        return new SimplePortable();
-                    } else {
-                        return null;
-                    }
-                }
-            };
-            return Client.newHazelcastClient(cfg);
+            return Client.newHazelcastClient(getClientConfig());
         }).then(function (cl) {
             client = cl;
             map = client.getMap('test');
@@ -43,12 +47,32 @@ describe('Default serializers with live instance', function() {
         return RC.shutdownCluster(cluster.id);
     });
 
-    it('two portables write and read', function () {
+    it('client can write and read two different serializable objects of the same factory', function () {
         var simplePortable = new SimplePortable('atext');
         var innerPortable = new InnerPortable('str1', 'str2');
         return map.put('simpleportable', simplePortable).then(function () {
             return map.put('innerportable', innerPortable);
         }).then(function() {
+            return map.get('simpleportable');
+        }).then(function (sp) {
+            return map.get('innerportable').then(function (ip) {
+                expect(sp).to.deep.equal(simplePortable);
+                expect(ip).to.deep.equal(innerPortable);
+                return Promise.resolve();
+            });
+        });
+    });
+
+    it('client can read two different serializable objects of the same factory (written by another client)', function () {
+        var simplePortable = new SimplePortable('atext');
+        var innerPortable = new InnerPortable('str1', 'str2');
+        return map.putAll([['simpleportable', simplePortable], ['innerportable', innerPortable]]).then(function () {
+            client.shutdown();
+        }).then(function() {
+            return Client.newHazelcastClient(getClientConfig());
+        }).then(function (cl) {
+            client = cl;
+            map = client.getMap('test');
             return map.get('simpleportable');
         }).then(function (sp) {
             return map.get('innerportable').then(function (ip) {
