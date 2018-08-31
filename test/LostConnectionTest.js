4a4990e13170a644a8cf3e0fced04a286a4bd088
furkansenharputlu <furkan_senharputlu@hotmail.com>
2018-08-31 21:33:54 +0300

    fix Lost connection test (#339)

--- test/LostConnectionTest.js
+++ test/LostConnectionTest.js
@@ -18,27 +18,26 @@ var Controller = require('./RC');
 var expect = require('chai').expect;
 var HazelcastClient = require('../.').Client;
 var Config = require('../.').Config;
+var Util = require('./Util');
+
 describe('Lost connection', function () {
     var cluster;
-    var member1;
+    var oldMember;
     var client;
-    before(function (done) {
-        Controller.createCluster(null, null).then(function (res) {
+    before(function () {
+        return Controller.createCluster(null, null).then(function (res) {
             cluster = res;
-            Controller.startMember(cluster.id).then(function (res) {
-                member1 = res;
-                var cfg = new Config.ClientConfig();
-                cfg.properties['hazelcast.client.heartbeat.interval'] = 500;
-                cfg.properties['hazelcast.client.heartbeat.timeout'] = 2000;
-                return HazelcastClient.newHazelcastClient(cfg);
-            }).then(function (res) {
-                client = res;
-                done();
-            }).catch(function (err) {
-                done(err);
-            });
-        }).catch(function (err) {
-            done(err);
+        }).then(function () {
+            return Controller.startMember(cluster.id);
+        }).then(function (m) {
+            oldMember = m;
+        }).then(function () {
+            var cfg = new Config.ClientConfig();
+            cfg.properties['hazelcast.client.heartbeat.interval'] = 500;
+            cfg.properties['hazelcast.client.heartbeat.timeout'] = 2000;
+            return HazelcastClient.newHazelcastClient(cfg);
+        }).then(function (cl) {
+            client = cl;
         });
     });
 
@@ -51,16 +50,16 @@ describe('Lost connection', function () {
         this.timeout(12000);
         var newMember;
         client.clusterService.on('memberAdded', function () {
-            Controller.shutdownMember(cluster.id, member1.uuid).then(function () {
-                setTimeout(function () {
-                    try {
-                        expect(client.clusterService.getOwnerConnection().address.host).to.be.eq(newMember.host);
-                        expect(client.clusterService.getOwnerConnection().address.port).to.be.eq(newMember.port);
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                }, 2500)
+            Controller.shutdownMember(cluster.id, oldMember.uuid).then(function () {
+                return Util.promiseWaitMilliseconds(4000);
+            }).then(function () {
+                try {
+                    expect(client.clusterService.getOwnerConnection().address.host).to.be.eq(newMember.host);
+                    expect(client.clusterService.getOwnerConnection().address.port).to.be.eq(newMember.port);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
             });
         });
         Controller.startMember(cluster.id).then(function (m) {
