3f6a719a128c0c0c8e9bc8e5f4650aebf46580d5
Furkan Şenharputlu <furkan_senharputlu@hotmail.com>
2018-11-30 14:30:53 +0300

    Update scripts and versions

--- scripts/dev-test-rc.sh
+++ scripts/dev-test-rc.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-HZ_VERSION="3.10"
-HAZELCAST_TEST_VERSION="3.10"
+HZ_VERSION="3.11.1-SNAPSHOT"
+HAZELCAST_TEST_VERSION="3.11.1-SNAPSHOT"
 HAZELCAST_VERSION=${HZ_VERSION}
 HAZELCAST_ENTERPRISE_VERSION=${HZ_VERSION}
+HAZELCAST_ENTERPRISE_TEST_VERSION=${HZ_VERSION}
 HAZELCAST_RC_VERSION="0.4-SNAPSHOT"
 SNAPSHOT_REPO="https://oss.sonatype.org/content/repositories/snapshots"
 RELEASE_REPO="http://repo1.maven.apache.org/maven2"
@@ -51,6 +52,18 @@ if [ -n "${HAZELCAST_ENTERPRISE_KEY}" ]; then
             exit 1
         fi
     fi
+
+    if [ -f "hazelcast-enterprise-${HAZELCAST_ENTERPRISE_TEST_VERSION}-tests.jar" ]; then
+        echo "hazelcast-enterprise-test.jar already exists, not downloading from maven."
+    else
+        echo "Downloading: hazelcast enterprise test jar com.hazelcast:hazelcast-enterprise:${HAZELCAST_ENTERPRISE_TEST_VERSION}:jar:tests"
+        mvn -q dependency:get -DrepoUrl=${ENTERPRISE_REPO} -Dartifact=com.hazelcast:hazelcast-enterprise:${HAZELCAST_ENTERPRISE_TEST_VERSION}:jar:tests -Ddest=hazelcast-enterprise-${HAZELCAST_ENTERPRISE_TEST_VERSION}-tests.jar
+        if [ $? -ne 0 ]; then
+            echo "Failed download hazelcast enterprise test jar com.hazelcast:hazelcast-enterprise:${HAZELCAST_ENTERPRISE_TEST_VERSION}:jar:tests"
+            exit 1
+        fi
+    fi
+
     CLASSPATH="hazelcast-remote-controller-${HAZELCAST_RC_VERSION}.jar:hazelcast-enterprise-${HAZELCAST_VERSION}.jar:hazelcast-${HAZELCAST_VERSION}.jar:hazelcast-${HAZELCAST_TEST_VERSION}-tests.jar:test/javaclasses"
     echo "Starting Remote Controller ... enterprise ..."
 else
