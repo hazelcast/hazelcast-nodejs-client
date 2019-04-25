7d50727112d9dfcbfd54244488b4f4ecd226322f
Metin Dumandağ <metin@hazelcast.com>
2019-04-25 16:41:14 +0300

    add server version check to hazelcast json value tests and increase timeout on listener tests for old server versions (#475)
    * add server version check to hazelcast json value tests and increase timeout on listener tests for old server versions
    
    * update hz version to 3.12.1 snapshot
    
    * seperate hazelcast json value query tests from basic storage tests

--- test/hazelcast_json_value/HazelcastJsonValueQueryTest.js
+++ test/hazelcast_json_value/HazelcastJsonValueQueryTest.js
@@ -19,16 +19,18 @@ chai.use(require('chai-as-promised'));
 var expect = require('chai').expect;
 var Client = require('../../.').Client;
 var RC = require('./../RC');
+var Predicates = require('../../.').Predicates;
 var HazelcastJsonValue = require('../../.').HazelcastJsonValue;
+var Util = require('../Util');
 
-describe('HazelcastJsonValue with JsonSerializer', function () {
+describe('HazelcastJsonValue query test', function () {
     var cluster;
     var client;
     var map;
     var object = { 'a': 1 };
-    var hzJsonValue = new HazelcastJsonValue(JSON.stringify(object));
 
     before(function () {
+        Util.markServerVersionAtLeast(this, null, '3.12');
         return RC.createCluster().then(function (response) {
             cluster = response;
             return RC.startMember(cluster.id);
@@ -50,59 +52,57 @@ describe('HazelcastJsonValue with JsonSerializer', function () {
     });
 
     after(function () {
+        if (!client) {
+            return;
+        }
         client.shutdown();
         return RC.shutdownCluster(cluster.id);
     });
 
-    it('storing JavaScript objects', function () {
-        return map.put(1, object).then(function () {
-            return map.get(1);
-        }).then(function (value) {
-            expect(value).to.deep.equal(object);
+    it('querying over JavaScript objects', function () {
+        var objects = [
+            [0, {'a': 1}],
+            [1, {'a': 3}]
+        ];
+        return map.putAll(objects).then(function () {
+            return map.valuesWithPredicate(Predicates.greaterThan('a', 2));
+        }).then(function (values) {
+            expect(values.toArray()).to.deep.equal([objects[1][1]]);
         });
     });
 
-    it('storing HazelcastJsonValue objects', function () {
-        return map.put(1, hzJsonValue).then(function () {
-            return map.get(1);
-        }).then(function (value) {
-            expect(value).to.deep.equal(object);
+    it('querying over nested attributes', function () {
+        var objects = [
+            [0, {'a': 1, 'b': {'c': 1}}],
+            [1, {'a': 3, 'b': {'c': 3}}]
+        ];
+        return map.putAll(objects).then(function () {
+            return map.valuesWithPredicate(Predicates.greaterThan('b.c', 2));
+        }).then(function (values) {
+            expect(values.toArray()).to.deep.equal([objects[1][1]]);
         });
     });
 
-    it('storing invalid Json strings', function () {
-        var invalidString = '{a}';
-        return map.put(1, new HazelcastJsonValue(invalidString)).then(function () {
-            return expect(map.get(1)).to.be.rejectedWith(SyntaxError);
-        });
-    });
-
-    it('storing JavaScript and HazelcastJsonValue objects as keys', function () {
+    it('querying over keys', function () {
+        var hzJsonValue2 = new HazelcastJsonValue('{ "a": 3 }');
         return map.put(object, 1).then(function () {
-            return map.get(object);
-        }).then(function (value) {
-            expect(value).to.equal(1);
-            return map.put(hzJsonValue, 2);
+            return map.put(hzJsonValue2, 2);
         }).then(function () {
-            return map.get(hzJsonValue);
-        }).then(function (value) {
-            expect(value).to.equal(2);
-            return map.size();
-        }).then(function (size) {
-            expect(size).to.equal(1);
+            return map.valuesWithPredicate(Predicates.sql('__key.a > 2'));
+        }).then(function (values) {
+            expect(values.toArray()).to.deep.equal([2]);
         });
     });
-    
-    it('storing JavaScript and HazelcastJsonValue objects together', function () {
-        return map.put(1, object).then(function () {
-            return map.put(2, hzJsonValue);
+
+    it('querying nested attributes over keys', function () {
+        var object1 = {'a': 1, 'b': {'c': 1}};
+        var object2 = {'a': 1, 'b': {'c': 3}};
+        return map.put(object1, 1).then(function () {
+            return map.put(object2, 2);
         }).then(function () {
-            return map.get(1);
-        }).then(function (value) {
-            expect(value).to.deep.equal(object);
-            return map.get(2);
-        }).then(function (value) {
-            expect(value).to.deep.equal(object);
+            return map.keySetWithPredicate(Predicates.equal('__key.b.c', 3));
+        }).then(function (keySet) {
+            expect(keySet).to.deep.equal([object2]);
         });
     });
 });
