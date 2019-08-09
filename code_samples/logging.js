ca6293a415000512e843548baa0f584c93d8e3af
Gulce Sirvanci <39351026+gulcesirvanci@users.noreply.github.com>
2019-08-09 09:51:10 +0300

    fixes logging sample and readme (#504)
    * fixes logging sample and readme
    
    * fixes for review
    
    * fixes for review
    
    * fix to readme
    
    * fixes

--- code_samples/logging.js
+++ code_samples/logging.js
@@ -19,59 +19,49 @@ var Config = require('hazelcast-client').Config;
 var HazelcastClient = require('hazelcast-client').Client;
 var LogLevel = require('hazelcast-client').LogLevel;
 
-if (process.argv.length != 3) {
-    console.log('Run as node logging.js [logger]');
-    console.log('[logger]: winston/default');
-} else {
-    var cfg = new Config.ClientConfig();
+var cfg = new Config.ClientConfig();
 
-    if (process.argv[2] === 'winston') {
-        var winstonAdapter = {
-            logger: new (winston.Logger)({
-                transports: [
-                    new (winston.transports.Console)()
-                ]
-            }),
+var winstonAdapter = {
+    logger: new (winston.Logger)({
+        transports: [
+            new (winston.transports.Console)()
+        ]
+    }),
 
-            levels: [
-                'error',
-                'warn',
-                'info',
-                'debug',
-                'silly'
-            ],
+    levels: [
+        'error',
+        'warn',
+        'info',
+        'debug',
+        'silly'
+    ],
 
-            log: function (level, objectName, message, furtherInfo) {
-                this.logger.log(this.levels[level], objectName + ': ' + message, furtherInfo);
-            },
+    log: function (level, objectName, message, furtherInfo) {
+        this.logger.log(this.levels[level], objectName + ': ' + message, furtherInfo);
+    },
 
-            error: function (objectName, message, furtherInfo) {
-                this.log(LogLevel.ERROR, objectName, message, furtherInfo);
-            },
+    error: function (objectName, message, furtherInfo) {
+        this.log(LogLevel.ERROR, objectName, message, furtherInfo);
+    },
 
-            debug: function (objectName, message, furtherInfo) {
-                this.log(LogLevel.DEBUG, objectName, message, furtherInfo);
-            },
+    warn: function (objectName, message, furtherInfo) {
+        this.log(LogLevel.WARN, objectName, message, furtherInfo);
+    },
 
-            warn: function (objectName, message, furtherInfo) {
-                this.log(LogLevel.WARN, objectName, message, furtherInfo);
-            },
+    info: function (objectName, message, furtherInfo) {
+        this.log(LogLevel.INFO, objectName, message, furtherInfo);
+    },
 
-            info: function (objectName, message, furtherInfo) {
-                this.log(LogLevel.INFO, objectName, message, furtherInfo);
-            },
+    debug: function (objectName, message, furtherInfo) {
+        this.log(LogLevel.DEBUG, objectName, message, furtherInfo);
+    },
 
-            trace: function (objectName, message, furtherInfo) {
-                this.log(LogLevel.TRACE, objectName, message, furtherInfo);
-            }
-        };
-        cfg.properties['hazelcast.logging'] = winstonAdapter;
-    } else {
-        cfg.properties['hazelcast.logging'] = process.argv[2];
-
+    trace: function (objectName, message, furtherInfo) {
+        this.log(LogLevel.TRACE, objectName, message, furtherInfo);
     }
-    HazelcastClient.newHazelcastClient(cfg).then(function (client) {
-        client.shutdown();
-    });
-}
+};
+cfg.customLogger = winstonAdapter;
 
+HazelcastClient.newHazelcastClient(cfg).then(function (client) {
+    client.shutdown();
+});
