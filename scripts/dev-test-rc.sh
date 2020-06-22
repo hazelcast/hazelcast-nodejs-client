ea8f5fb98a93c222ada7f8b1a89e8447a313346f
Metin Dumandag <metin@hazelcast.com>
2020-06-22 17:02:17 +0300

    Fix ListenerServiceTest
    This test fails time to time. I fixed the issue with this test in
    master branch. This PR backports the changes done in master to
    3.12.x branch.
    
    The problem is the following. In tests, after asserting the
    values received from the events, we remove the distributed object
    listener and call done(). However, done is called in the next
    statment instead of a then block. Because of that, sometimes,
    the listeners from the previous test runs stay active in other tests,
    and that causes the failures. We could fix the individual tests but
    I believe that we should not use the same client for every test.
    Each of them tests a separate logic. Therefore, in this fix I changed
    the tests so that each of them uses a separate client.

--- scripts/dev-test-rc.sh
+++ scripts/dev-test-rc.sh
@@ -18,6 +18,15 @@ else
 	ENTERPRISE_REPO=${ENTERPRISE_RELEASE_REPO}
 fi
 
+if [[ ${HZ_TEST_VERSION} == *-SNAPSHOT ]]
+then
+    TEST_REPO=${SNAPSHOT_REPO}
+    ENTRERPRISE_TEST_REPO=${ENTERPRISE_SNAPSHOT_REPO}
+else
+    TEST_REPO=${RELEASE_REPO}
+    ENTRERPRISE_TEST_REPO=${ENTERPRISE_RELEASE_REPO}
+fi
+
 if [ -f "hazelcast-remote-controller-${HAZELCAST_RC_VERSION}.jar" ]; then
     echo "remote controller already exist, not downloading from maven."
 else
@@ -56,7 +65,7 @@ if [ -n "${HAZELCAST_ENTERPRISE_KEY}" ]; then
         echo "hazelcast-enterprise-tests.jar already exists, not downloading from maven."
     else
         echo "Downloading: hazelcast enterprise test jar com.hazelcast:hazelcast-enterprise:${HAZELCAST_TEST_VERSION}:jar:tests"
-        mvn -q org.apache.maven.plugins:maven-dependency-plugin:2.8:get -DrepoUrl=${ENTERPRISE_REPO} -Dartifact=com.hazelcast:hazelcast-enterprise:${HAZELCAST_TEST_VERSION}:jar:tests -Ddest=hazelcast-enterprise-${HAZELCAST_TEST_VERSION}-tests.jar
+        mvn -q org.apache.maven.plugins:maven-dependency-plugin:2.8:get -DrepoUrl=${ENTRERPRISE_TEST_REPO} -Dartifact=com.hazelcast:hazelcast-enterprise:${HAZELCAST_TEST_VERSION}:jar:tests -Ddest=hazelcast-enterprise-${HAZELCAST_TEST_VERSION}-tests.jar
         if [ $? -ne 0 ]; then
             echo "Failed to download hazelcast enterprise test jar com.hazelcast:hazelcast-enterprise:${HAZELCAST_TEST_VERSION}:jar:tests"
             exit 1
@@ -81,4 +90,3 @@ else
 fi
 
 java -Dhazelcast.enterprise.license.key=${HAZELCAST_ENTERPRISE_KEY} -cp ${CLASSPATH} com.hazelcast.remotecontroller.Main
-
