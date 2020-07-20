3e205a5939a7a398e83ba686ce1c6b79d8dfafec
Andrey Pechkurov <apechkurov@gmail.com>
2020-07-20 15:03:11 +0300

    Add MultiMapPutAllCodec

---
+++ src/codec/MultiMapPutAllCodec.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable max-len */
+import {BitsUtil} from '../BitsUtil';
+import {ClientMessage, Frame, PARTITION_ID_OFFSET} from '../ClientMessage';
+import {StringCodec} from './builtin/StringCodec';
+
+// hex: 0x021700
+const REQUEST_MESSAGE_TYPE = 136960;
+// hex: 0x021701
+// RESPONSE_MESSAGE_TYPE = 136961
+
+const REQUEST_INITIAL_FRAME_SIZE = PARTITION_ID_OFFSET + BitsUtil.INT_SIZE_IN_BYTES;
+
+export class MultiMapPutAllCodec {
+    static encodeRequest(name: string, entries: Array<[Data, Data[]]>): ClientMessage {
+        const clientMessage = ClientMessage.createForEncode();
+        clientMessage.setRetryable(false);
+
+        const initialFrame = Frame.createInitialFrame(REQUEST_INITIAL_FRAME_SIZE);
+        clientMessage.addFrame(initialFrame);
+        clientMessage.setMessageType(REQUEST_MESSAGE_TYPE);
+        clientMessage.setPartitionId(-1);
+
+        StringCodec.encode(clientMessage, name);
+        EntryListCodec.encode(clientMessage, entries, DataCodec.encode, ListDataCodec.encode);
+        return clientMessage;
+    }
+}
