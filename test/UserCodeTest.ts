c9571a9eda57a7c8a17cc7ec645153488e0c7ee3
Andrey Pechkurov <apechkurov@gmail.com>
2020-08-11 12:31:30 +0300

    Add test for declarations produced by tsc

---
+++ test/UserCodeTest.ts
@@ -0,0 +1,13 @@
+/**
+ * The following test validates correctness of
+ * declarations (.d.ts) of the client library
+ * produced by TypeScript compiler.
+ */
+
+import { Client } from '../lib';
+
+(async () => {
+    const client = await Client.newHazelcastClient();
+    const rb = await client.getRingbuffer('my-rb');
+    await rb.add(1);
+})();
