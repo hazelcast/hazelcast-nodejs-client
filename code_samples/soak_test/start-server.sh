d85b0a0450afe827d6c5ca4215609a074d623237
Andrey Pechkurov <apechkurov@gmail.com>
2020-09-23 11:17:39 +0300

    Port soak test
    Also includes the following:
    * Updates TS version to latest 2.x
    * Updates non-dev dependencies to latest versions
    * Updates mocha version

---
+++ code_samples/soak_test/start-server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+HAZELCAST_TEST_VERSION="3.12.9"
+HAZELCAST_VERSION="3.12.9"
+PID=$$
+
+CLASSPATH="../../hazelcast-${HAZELCAST_VERSION}.jar:../../hazelcast-${HAZELCAST_TEST_VERSION}-tests.jar"
+CMD_CONFIGS="-Dhazelcast.multicast.group=224.206.1.1 -Djava.net.preferIPv4Stack=true"
+java ${CMD_CONFIGS} -cp ${CLASSPATH} \
+    com.hazelcast.core.server.StartServer \
+    > hazelcast-${HAZELCAST_VERSION}-${PID}-out.log 2>hazelcast-${HAZELCAST_VERSION}-${PID}-err.log &
