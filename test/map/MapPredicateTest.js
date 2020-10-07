d7c1fb64021b9ae4b3cb797dc544b5603b3a51bf
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-07 09:17:11 +0530

    migrated some more tests for map

--- test/map/MapPredicateTest.js
+++ test/map/MapPredicateTest.js
@@ -19,8 +19,8 @@ const { expect } = require('chai');
 const assert = require('assert');
 const fs = require('fs');
 
-const RC = require('./../RC');
-const { Client, Predicates } = require('../..');
+const RC = require('../RC');
+const { Client, Predicates } = require('../../lib');
 const identifiedFactory = require('../javaclasses/IdentifiedFactory');
 const CustomComparator = require('../javaclasses/CustomComparator');
 
@@ -34,42 +34,32 @@ describe('MapPredicateTest', function () {
         return new CustomComparator(1, Predicates.IterationType.ENTRY);
     }
 
-    before(function () {
+    before(async function () {
         this.timeout(32000);
-        return RC.createCluster(null, fs.readFileSync(__dirname + '/hazelcast_identifiedfactory.xml', 'utf8'))
-            .then(function (res) {
-                cluster = res;
-                return RC.startMember(cluster.id);
-            })
-            .then(function (member) {
-                return Client.newHazelcastClient({
-                    clusterName: cluster.id,
-                    serialization: {
-                        dataSerializableFactories: {
-                            66: identifiedFactory
-                        }
-                    }
-                })
-            })
-            .then(function (hazelcastClient) {
-                client = hazelcastClient;
-            });
+        cluster = await RC.createCluster(null, fs.readFileSync(__dirname + '/hazelcast_identifiedfactory.xml', 'utf8'));
+        await RC.startMember(cluster.id);
+        client = await Client.newHazelcastClient({
+            clusterName: cluster.id,
+            serialization: {
+                dataSerializableFactories: {
+                    66: identifiedFactory
+                }
+            }
+        });
     });
 
-    beforeEach(function () {
-        return client.getMap('test').then(function (mp) {
-            map = mp;
-            return fillMap();
-        });
+    beforeEach(async function () {
+        map = await client.getMap('test');
+        await fillMap();
     });
 
     afterEach(function () {
         return map.destroy();
     });
 
-    after(function () {
-        return client.shutdown()
-            .then(() => RC.terminateCluster(cluster.id));
+    after(async function () {
+        await client.shutdown()
+        return RC.terminateCluster(cluster.id);
     });
 
     function fillMap(size) {
@@ -83,146 +73,130 @@ describe('MapPredicateTest', function () {
         return Promise.all(promises);
     }
 
-    function testPredicate(predicate, expecteds, orderMatters) {
-        return map.valuesWithPredicate(predicate).then(function (values) {
-            if (orderMatters) {
-                return expect(values.toArray()).to.deep.equal(expecteds);
-            } else {
-                return expect(values.toArray()).to.have.members(expecteds);
-            }
-        });
+    async function testPredicate(predicate, expecteds, orderMatters) {
+        let values = await map.valuesWithPredicate(predicate);
+        if (orderMatters) {
+            return expect(values.toArray()).to.deep.equal(expecteds);
+        } else {
+            return expect(values.toArray()).to.have.members(expecteds);
+        }
     }
 
-    it('Sql', function () {
-        return testPredicate(Predicates.sql('this == 10'), [10]);
+    it('Sql', async function () {
+        await testPredicate(Predicates.sql('this == 10'), [10]);
     });
 
-    it('And', function () {
-        return testPredicate(Predicates.and(Predicates.equal('this', 10), Predicates.equal('this', 11)), []);
+    it('And', async function () {
+        await testPredicate(Predicates.and(Predicates.equal('this', 10), Predicates.equal('this', 11)), []);
     });
 
-    it('GreaterThan', function () {
-        return testPredicate(Predicates.greaterThan('this', 47), [48, 49]);
+    it('GreaterThan', async function () {
+        await testPredicate(Predicates.greaterThan('this', 47), [48, 49]);
     });
 
-    it('GreaterEqual', function () {
-        return testPredicate(Predicates.greaterEqual('this', 47), [47, 48, 49]);
+    it('GreaterEqual', async function () {
+        await testPredicate(Predicates.greaterEqual('this', 47), [47, 48, 49]);
     });
 
-    it('LessThan', function () {
-        return testPredicate(Predicates.lessThan('this', 4), [0, 1, 2, 3]);
+    it('LessThan', async function () {
+        await testPredicate(Predicates.lessThan('this', 4), [0, 1, 2, 3]);
     });
 
-    it('LessEqual', function () {
-        return testPredicate(Predicates.lessEqual('this', 4), [0, 1, 2, 3, 4]);
+    it('LessEqual', async function () {
+        await testPredicate(Predicates.lessEqual('this', 4), [0, 1, 2, 3, 4]);
     });
 
-    it('Like', function () {
-        let localMap;
-        return client.getMap('likePredMap').then(function (mp) {
-            localMap = mp;
-            return localMap.put('temp', 'tempval');
-        }).then(function () {
-            return localMap.valuesWithPredicate(Predicates.like('this', 'tempv%'));
-        }).then(function (values) {
-            return expect(values.toArray()).to.have.members(['tempval']);
-        }).then(function () {
-            return localMap.destroy();
-        });
+    it('Like', async function () {
+        let localMap = await client.getMap('likePredMap');
+        await localMap.put('temp', 'tempval');
+        let values = await localMap.valuesWithPredicate(Predicates.like('this', 'tempv%'));
+        expect(values.toArray()).to.have.members(['tempval']);
+        await localMap.destroy();
     });
 
-    it('ILike', function () {
-        let localMap;
-        return client.getMap('likePredMap').then(function (mp) {
-            localMap = mp;
-            return localMap.putAll([['temp', 'tempval'], ['TEMP', 'TEMPVAL']]);
-        }).then(function () {
-            return localMap.valuesWithPredicate(Predicates.ilike('this', 'tempv%'));
-        }).then(function (values) {
-            return expect(values.toArray()).to.have.members(['tempval', 'TEMPVAL']);
-        }).then(function () {
-            return localMap.destroy();
-        });
+    it('ILike', async function () {
+        let localMap = await client.getMap('likePredMap');
+        await localMap.putAll([['temp', 'tempval'], ['TEMP', 'TEMPVAL']]);
+        let values = await localMap.valuesWithPredicate(Predicates.ilike('this', 'tempv%'));
+        expect(values.toArray()).to.have.members(['tempval', 'TEMPVAL']);
+        await localMap.destroy();
     });
 
-    it('In', function () {
-        return testPredicate(Predicates.inPredicate('this', 48, 49, 50, 51, 52), [48, 49]);
+    it('In', async function () {
+        await testPredicate(Predicates.inPredicate('this', 48, 49, 50, 51, 52), [48, 49]);
     });
 
-    it('InstanceOf', function () {
-        const assertionList = Array.apply(null, {length: 50}).map(Number.call, Number);
-        return testPredicate(Predicates.instanceOf('java.lang.Double'), assertionList);
+    it('InstanceOf', async function () {
+        const assertionList = Array.apply(null, { length: 50 }).map(Number.call, Number);
+        await testPredicate(Predicates.instanceOf('java.lang.Double'), assertionList);
     });
 
-    it('NotEqual', function () {
-        const assertionList = Array.apply(null, {length: 49}).map(Number.call, Number);
-        return testPredicate(Predicates.notEqual('this', 49), assertionList);
+    it('NotEqual', async function () {
+        const assertionList = Array.apply(null, { length: 49 }).map(Number.call, Number);
+        await testPredicate(Predicates.notEqual('this', 49), assertionList);
     });
 
-    it('Not', function () {
-        return testPredicate(Predicates.not(Predicates.greaterEqual('this', 2)), [0, 1]);
+    it('Not', async function () {
+        await testPredicate(Predicates.not(Predicates.greaterEqual('this', 2)), [0, 1]);
     });
 
-    it('Or', function () {
-        return testPredicate(
+    it('Or', async function () {
+        await testPredicate(
             Predicates.or(Predicates.greaterEqual('this', 49), Predicates.lessEqual('this', 0)),
             [0, 49]
         );
     });
 
-    it('Between', function () {
-        return testPredicate(Predicates.between('this', 47, 49), [47, 48, 49]);
+    it('Between', async function () {
+        await testPredicate(Predicates.between('this', 47, 49), [47, 48, 49]);
     });
 
-    it('Null predicate throws error', function () {
-        return expect(testPredicate.bind(null, null, [0, 49])).throw(assert.AssertionError);
+    it('Null predicate throws error', async function () {
+        const values = async function () {
+            await testPredicate(null, null, [0, 49]);
+        }
+        assert.rejects(values);
     });
 
-    it('Regex', function () {
-        let localMap;
-        return client.getMap('regexMap').then(function (mp) {
-            localMap = mp;
-            return localMap.putAll([['06', 'ankara'], ['07', 'antalya']])
-        }).then(function () {
-            return localMap.valuesWithPredicate(Predicates.regex('this', '^.*ya$'));
-        }).then(function (values) {
-            return expect(values.toArray()).to.have.members(['antalya']);
-        }).then(function () {
-            return localMap.destroy();
-        });
+    it('Regex', async function () {
+        let localMap = await client.getMap('regexMap');
+        await localMap.putAll([['06', 'ankara'], ['07', 'antalya']])
+        let values = await localMap.valuesWithPredicate(Predicates.regex('this', '^.*ya$'));
+        expect(values.toArray()).to.have.members(['antalya']);
+        await localMap.destroy();
     });
 
-    it('False', function () {
-        return testPredicate(Predicates.alwaysFalse(), []);
+    it('False', async function () {
+        await testPredicate(Predicates.alwaysFalse(), []);
     });
 
-    it('True', function () {
-        const assertionList = Array.apply(null, {length: 50}).map(Number.call, Number);
-        return testPredicate(Predicates.alwaysTrue(), assertionList);
+    it('True', async function () {
+        const assertionList = Array.apply(null, { length: 50 }).map(Number.call, Number);
+        await testPredicate(Predicates.alwaysTrue(), assertionList);
     });
 
-    it('Paging with reverse comparator should have elements in reverse order', function () {
+    it('Paging with reverse comparator should have elements in reverse order', async function () {
         const paging = Predicates.paging(Predicates.lessThan('this', 10), 3, createReverseValueComparator());
-        return testPredicate(paging, [9, 8, 7], true);
+        await testPredicate(paging, [9, 8, 7], true);
     });
 
-    it('Paging first page should have first two items', function () {
+    it('Paging first page should have first two items', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 40), 2);
-        return testPredicate(paging, [40, 41]);
+        await testPredicate(paging, [40, 41]);
     });
 
-    it('Paging nextPage should have 3rd and 4th items', function () {
+    it('Paging nextPage should have 3rd and 4th items', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 40), 2);
         paging.nextPage();
 
-        return testPredicate(paging, [42, 43]);
+        await testPredicate(paging, [42, 43]);
     });
 
-    it('Paging fourth page should have 7th and 8th items', function () {
+    it('Paging fourth page should have 7th and 8th items', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 40), 2);
         paging.setPage(4);
 
-        return testPredicate(paging, [48, 49]);
+        await testPredicate(paging, [48, 49]);
     });
 
     it('Paging #getPage should return approprate value', function () {
@@ -236,53 +210,51 @@ describe('MapPredicateTest', function () {
         return expect(paging.getPageSize()).to.equal(2);
     });
 
-    it('Paging previousPage', function () {
+    it('Paging previousPage', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 40), 2);
         paging.setPage(4);
         paging.previousPage();
 
-        return testPredicate(paging, [46, 47]);
+        await testPredicate(paging, [46, 47]);
     });
 
-    it('Get 4th page, then previous page', function () {
+    it('Get 4th page, then previous page', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 40), 2);
         paging.setPage(4);
-        return map.valuesWithPredicate(paging).then(function () {
-            paging.previousPage();
-            return testPredicate(paging, [46, 47]);
-        });
+        await map.valuesWithPredicate(paging);
+        paging.previousPage();
+        await testPredicate(paging, [46, 47]);
     });
 
-    it('Get 3rd page, then next page', function () {
+    it('Get 3rd page, then next page', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 40), 2);
         paging.setPage(3);
-        return map.valuesWithPredicate(paging).then(function () {
-            paging.nextPage();
-            return testPredicate(paging, [48, 49]);
-        });
+        await map.valuesWithPredicate(paging);
+        paging.nextPage();
+        await testPredicate(paging, [48, 49]);
     });
 
-    it('Get 10th page (which does not exist) should return empty list', function () {
+    it('Get 10th page (which does not exist) should return empty list', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 40), 2);
         paging.setPage(10);
 
-        return testPredicate(paging, []);
+        await testPredicate(paging, []);
     });
 
-    it('Last page has only one element although page size is 2', function () {
+    it('Last page has only one element although page size is 2', async function () {
         const paging = Predicates.paging(Predicates.greaterEqual('this', 41), 2);
         paging.setPage(4);
 
-        return testPredicate(paging, [49]);
+        await testPredicate(paging, [49]);
     });
 
-    it('There is no element satisfying paging predicate returns empty array', function () {
+    it('There is no element satisfying paging predicate returns empty array', async function () {
         const paging = Predicates.paging(Predicates.lessThan('this', 0), 2);
-        return testPredicate(paging, []);
+        await testPredicate(paging, []);
     });
 
-    it('Null inner predicate in PagingPredicate does not filter out items, only does paging', function () {
+    it('Null inner predicate in PagingPredicate does not filter out items, only does paging', async function () {
         const paging = Predicates.paging(null, 2);
-        return testPredicate(paging, [0, 1]);
+        await testPredicate(paging, [0, 1]);
     });
 });
