c1287bc3cb1fd9904ba9eddd7dabe0b82c5cdc3a
Andrey Pechkurov <apechkurov@gmail.com>
2020-11-11 11:12:07 +0300

    Cover murmurhash3 implementation with unit tests
    Also improves code style

--- src/invocation/Murmur.ts
+++ src/invocation/Murmur.ts
@@ -15,20 +15,23 @@
  */
 /** @ignore *//** */
 
-const seed = 0x01000193;
-
-/** @internal */
-export function murmur(key: any): number {
-    let h1: number, h1b: number, k1: number, i: number;
+/**
+ * murmur3 (MurmurHash3_x86_32) hash function to determine partition
+ * @internal
+ */
+export function murmur(key: Buffer): number {
+    let h1 = 0x01000193; // seed
+    let h1b: number;
+    let k1: number;
 
     const remainder = key.length & 3; // key.length % 4
     const bytes = key.length - remainder;
-    h1 = seed;
     const c1 = 0xcc9e2d51;
     const c2 = 0x1b873593;
-    i = 0;
 
+    let i = 0;
     while (i < bytes) {
+        // little-endian load order
         k1 =
             ((key.readUInt8(i) & 0xff)) |
             ((key.readUInt8(++i) & 0xff) << 8) |
@@ -37,31 +40,38 @@ export function murmur(key: any): number {
         ++i;
 
         k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+        // ROTL32(k1,15);
         k1 = (k1 << 15) | (k1 >>> 17);
         k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
 
         h1 ^= k1;
+        // ROTL32(h1,13);
         h1 = (h1 << 13) | (h1 >>> 19);
         h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
         h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
     }
 
+    // tail
     k1 = 0;
 
     switch (remainder) {
         case 3:
             k1 ^= (key.readUInt8(i + 2) & 0xff) << 16;
+            // fallthrough
         case 2: // eslint-disable-line no-fallthrough
             k1 ^= (key.readUInt8(i + 1) & 0xff) << 8;
+            // fallthrough
         case 1: // eslint-disable-line no-fallthrough
             k1 ^= (key.readUInt8(i) & 0xff);
 
             k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
+            // ROTL32(k1,15);
             k1 = (k1 << 15) | (k1 >>> 17);
             k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
             h1 ^= k1;
     }
 
+    // finalization
     h1 ^= key.length;
 
     h1 ^= h1 >>> 16;
