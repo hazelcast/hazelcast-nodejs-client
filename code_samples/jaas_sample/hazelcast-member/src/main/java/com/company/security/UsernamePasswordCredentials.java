906526977d347783cf9a93aee58ed1e992aabec4
Andrey Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2020-11-16 18:23:08 +0300

    Update Java snippets to 4.0 API (#669)

--- code_samples/jaas_sample/hazelcast-member/src/main/java/com/company/security/UsernamePasswordCredentials.java
+++ code_samples/jaas_sample/hazelcast-member/src/main/java/com/company/security/UsernamePasswordCredentials.java
@@ -8,6 +8,7 @@ import com.hazelcast.security.Credentials;
 import java.io.IOException;
 
 public class UsernamePasswordCredentials implements Credentials, Portable {
+
     public static final int CLASS_ID = 1;
 
     private String username;
@@ -17,30 +18,19 @@ public class UsernamePasswordCredentials implements Credentials, Portable {
     public UsernamePasswordCredentials() {
     }
 
-    public UsernamePasswordCredentials(String username, String password, String endpoint) {
-        this.username = username;
-        this.password = password;
-        this.endpoint = endpoint;
-    }
-
-    @Override
     public String getEndpoint() {
         return endpoint;
     }
 
-    @Override
-    public void setEndpoint(String endpoint) {
-        this.endpoint = endpoint;
-    }
-
-    @Override
-    public String getPrincipal() {
-        return getUsername();
-    }
-
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
+
+    @Override
+    public String getName() {
+        return username;
+    }
+
     public String getPassword() {
         return password;
     }
