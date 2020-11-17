2717baddfa18e40eac1e2412605e180d708c9410
Andrey Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2020-11-17 11:00:34 +0300

    Update Java snippets to 4.0 API (#675)

--- code_samples/jaas_sample/hazelcast-member/src/main/java/com/company/security/ClientLoginModule.java
+++ code_samples/jaas_sample/hazelcast-member/src/main/java/com/company/security/ClientLoginModule.java
@@ -1,19 +1,24 @@
 package com.company.security;
 
-import com.hazelcast.security.ClusterPrincipal;
+import com.hazelcast.security.ClusterEndpointPrincipal;
+import com.hazelcast.security.ClusterRolePrincipal;
 import com.hazelcast.security.Credentials;
 import com.hazelcast.security.CredentialsCallback;
+import com.hazelcast.security.TokenCredentials;
+import com.hazelcast.security.TokenDeserializerCallback;
 
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.security.auth.login.LoginException;
 import javax.security.auth.spi.LoginModule;
-import java.security.Principal;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class ClientLoginModule implements LoginModule {
+
     private UsernamePasswordCredentials usernamePasswordCredentials;
     private Subject subject;
     private CallbackHandler callbackHandler;
@@ -37,28 +42,36 @@ public class ClientLoginModule implements LoginModule {
         return true;
     }
 
-    public boolean abort() throws LoginException {
+    public boolean abort() {
         clearSubject();
         return true;
     }
 
     @Override
-    public boolean logout() throws LoginException {
+    public boolean logout() {
         clearSubject();
         return true;
     }
 
     private UsernamePasswordCredentials getCredentials() throws LoginException {
-        final CredentialsCallback cb = new CredentialsCallback();
-        Credentials credentials;
+        CredentialsCallback credcb = new CredentialsCallback();
+        // Warning: this callback is used for code conciseness purposes.
+        // We strongly recommend to avoid object deserialization during the authentication
+        // to prevent serialization based attacks.
+        TokenDeserializerCallback tdcb = new TokenDeserializerCallback();
         try {
-            callbackHandler.handle(new Callback[]{cb});
-            credentials = cb.getCredentials();
-        } catch (Exception e) {
-            throw new LoginException(e.getClass().getName() + ":" + e.getMessage());
+            callbackHandler.handle(new Callback[] { credcb, tdcb });
+        } catch (IOException | UnsupportedCallbackException e) {
+            throw new LoginException("Unable to retrieve necessary data");
         }
+        Credentials credentials = credcb.getCredentials();
         if (credentials == null) {
             throw new LoginException("Credentials could not be retrieved!");
+        }
+
+        if (credentials instanceof TokenCredentials) {
+            TokenCredentials tokenCreds = (TokenCredentials) credentials;
+            credentials = (Credentials) tdcb.getTokenDeserializer().deserialize(tokenCreds);
         }
 
         if (credentials instanceof UsernamePasswordCredentials) {
@@ -79,8 +92,8 @@ public class ClientLoginModule implements LoginModule {
         List<String> userGroups = authenticator.getRoles(usernamePasswordCredentials.getUsername());
         if (userGroups != null) {
             for (String userGroup : userGroups) {
-                Principal principal = new ClusterPrincipal(new UserGroupCredentials(usernamePasswordCredentials.getEndpoint(), userGroup));
-                subject.getPrincipals().add(principal);
+                subject.getPrincipals().add(new ClusterEndpointPrincipal(usernamePasswordCredentials.getEndpoint()));
+                subject.getPrincipals().add(new ClusterRolePrincipal(userGroup));
             }
         } else {
             throw new LoginException("User Group(s) not found for user " + usernamePasswordCredentials.getUsername());
