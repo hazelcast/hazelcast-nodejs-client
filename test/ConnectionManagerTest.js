f578b752abc5dae6ee955fe4659d24d2b560327f
Andrey Pechkurov <apechkurov@gmail.com>
2020-12-25 14:57:18 +0300

    Make sure to close socket on connection timeout
    Connection manager was not closing the underlying connection
    and socket in case of connection timeout. This could potentially
    lead to socket leaks.
    
    Also includes some minor improvements in tests.

--- test/ConnectionManagerTest.js
+++ test/ConnectionManagerTest.js
@@ -20,11 +20,13 @@ chai.should();
 chai.use(require('chai-as-promised'));
 const expect = chai.expect;
 const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
 const net = require('net');
 
 const RC = require('./RC');
 const { Client, IllegalStateError } = require('../');
 const { AddressImpl } = require('../lib/core/Address');
+const { ClientConnection } = require('../lib/network/ClientConnection');
 const { promiseWaitMilliseconds } = require('./Util');
 
 /**
@@ -56,6 +58,7 @@ describe('ConnectionManagerTest', function () {
     });
 
     beforeEach(function () {
+        sandbox.restore();
         testend = false;
     });
 
@@ -72,8 +75,9 @@ describe('ConnectionManagerTest', function () {
     });
 
     it('should give up connecting after timeout', async function () {
-        const timeoutTime = 1000;
         await startUnresponsiveServer(9999);
+
+        const timeoutTime = 1000;
         client = await Client.newHazelcastClient({
             clusterName: cluster.id,
             network: {
@@ -85,6 +89,49 @@ describe('ConnectionManagerTest', function () {
         await expect(
             connectionManager.getOrConnectToAddress(new AddressImpl('localhost', 9999))
         ).to.be.rejected;
+    });
+
+    it('closes connection after initial communication timeout', async function () {
+        await startUnresponsiveServer(9999);
+
+        const timeoutTime = 1000;
+        client = await Client.newHazelcastClient({
+            clusterName: cluster.id,
+            network: {
+                connectionTimeout: timeoutTime
+            }
+        });
+
+        const closeConnSpy = sandbox.spy(ClientConnection.prototype, 'close');
+
+        const connectionManager = client.getConnectionManager();
+        await expect(
+            connectionManager.getOrConnectToAddress(new AddressImpl('localhost', 9999))
+        ).to.be.rejected;
+
+        expect(closeConnSpy.calledOnce).to.be.true;
+    });
+
+    it('destroys socket after connection timeout', async function () {
+        const timeoutTime = 1000;
+        client = await Client.newHazelcastClient({
+            clusterName: cluster.id,
+            network: {
+                connectionTimeout: timeoutTime
+            }
+        });
+
+        // emulate connection timeout with a stub socket
+        const socketStub = sandbox.stub(net.Socket.prototype);
+        sandbox.stub(net, 'connect').returns(socketStub);
+
+        const connectionManager = client.getConnectionManager();
+        await expect(
+            connectionManager.getOrConnectToAddress(new AddressImpl('localhost', 9999))
+        ).to.be.rejected;
+
+        await promiseWaitMilliseconds(100);
+        expect(socketStub.destroy.callCount).to.be.equal(1);
     });
 
     it('should not give up when timeout set to 0', function (done) {
@@ -140,7 +187,7 @@ describe('ConnectionManagerTest', function () {
         const conn = await client.getConnectionManager().getOrConnectToAddress(new AddressImpl('localhost', 5701));
         expect(conn.isAlive()).to.be.true;
 
-        const closeSpy = sinon.spy(conn, 'close');
+        const closeSpy = sandbox.spy(conn, 'close');
         conn.socket.emit('error', new Error('boom'));
 
         expect(conn.isAlive()).to.be.false;
