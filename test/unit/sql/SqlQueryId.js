5401005a20920e854169351cff68180d88d97d1c
Serkan Özel <serkanozel380@gmail.com>
2021-04-21 18:11:07 +0300

    Update sqlQueryId.fromMemberId test to mock random generation of local UUID

--- test/unit/sql/SqlQueryId.js
+++ test/unit/sql/SqlQueryId.js
@@ -18,19 +18,23 @@
 const { expect } = require('chai');
 const { SqlQueryId } = require('../../../lib/sql/SqlQueryId');
 const { UuidUtil } = require('../../../lib/util/UuidUtil');
-const long = require('long');
+const sinon = require('sinon');
 
 describe('SqlQueryIdTest', function () {
-    const uuid = UuidUtil.generate();
-    const staticInstance = SqlQueryId.fromMemberId(uuid);
 
     describe('fromMemberId', function () {
         it('construct SqlQueryId correctly', function () {
-            expect(staticInstance.memberIdHigh.eq(uuid.mostSignificant)).to.be.true;
-            expect(staticInstance.memberIdLow.eq(uuid.leastSignificant)).to.be.true;
-            // local uuid is random uuid
-            expect(long.isLong(staticInstance.localIdHigh)).to.be.true;
-            expect(long.isLong(staticInstance.localIdLow)).to.be.true;
+            const memberUuid = UuidUtil.generate();
+            const localUuid = UuidUtil.generate();
+
+            sinon.replace(UuidUtil, 'generate', sinon.fake.returns(localUuid)); // mock random generation
+            const staticInstance = SqlQueryId.fromMemberId(memberUuid);
+
+            expect(staticInstance.memberIdHigh.eq(memberUuid.mostSignificant)).to.be.true;
+            expect(staticInstance.memberIdLow.eq(memberUuid.leastSignificant)).to.be.true;
+            expect(staticInstance.localIdHigh.eq(localUuid.mostSignificant)).to.be.true;
+            expect(staticInstance.localIdLow.eq(localUuid.leastSignificant)).to.be.true;
+            expect(UuidUtil.generate.calledOnce).to.be.true;
         });
     });
 });
