5ee339adf5ed998eb7e56442a80cde5bb328706f
Serkan Özel <serkan.ozel@hazelcast.com>
2021-10-15 11:52:54 +0300

    Remove main page

--- fetch-latest-homepage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Run this every time README.md in master changes
-
-wget https://raw.githubusercontent.com/hazelcast/hazelcast-nodejs-client/master/README.md
-touch index.md
-printf '%s\n%s\n%s\n' '---' 'layout: home' '---' >> index.md
-cat README.md >> index.md
-rm README.md
