1dc5d13e5d03f79042651aa2f8c340eb3d435de6
Serkan Özel <serkan.ozel@hazelcast.com>
2022-01-12 14:11:15 +0300

    Make simple credentials back compatible

--- test/integration/backward_compatible/parallel/security/SimpleCredentials.js
+++ test/integration/backward_compatible/parallel/security/SimpleCredentials.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 'use strict';
+const TestUtil = require('../../../../TestUtil');
 
 class SimpleCredentials {
     constructor(username, password) {
@@ -24,13 +25,25 @@ class SimpleCredentials {
     }
 
     readData(input) {
-        this.username = input.readString();
-        this.password = input.readString();
+        // readString is added in 4.2 https://github.com/hazelcast/hazelcast-nodejs-client/pull/802
+        if (TestUtil.isClientVersionAtLeast('4.2')) {
+            this.username = input.readString();
+            this.password = input.readString();
+        } else {
+            this.username = input.readUTF();
+            this.password = input.readUTF();
+        }
     }
 
     writeData(output) {
-        output.writeString(this.username);
-        output.writeString(this.password);
+        // writeString is added in 4.2 https://github.com/hazelcast/hazelcast-nodejs-client/pull/802
+        if (TestUtil.isClientVersionAtLeast('4.2')) {
+            output.writeString(this.username);
+            output.writeString(this.password);
+        } else {
+            output.writeUTF(this.username);
+            output.writeUTF(this.password);
+        }
     }
 }
 
