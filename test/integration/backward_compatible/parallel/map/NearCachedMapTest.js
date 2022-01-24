1b497d4f71fb6d9209dc58ead8aeecbd7c35d2ad
Serkan Özel <serkanozel380@gmail.com>
2022-01-24 04:17:29 +0300

    Use assertTrueEventually in NearCacheMapTest to avoid race

--- test/integration/backward_compatible/parallel/map/NearCachedMapTest.js
+++ test/integration/backward_compatible/parallel/map/NearCachedMapTest.js
@@ -96,15 +96,17 @@ describe('NearCachedMapTest', function () {
                 expectStats(map1, 0, 2, 1);
             });
 
-            it('get returns null if the entry was removed by another client', async function () {
+            it('get returns null eventually after the entry is removed by another client', async function () {
                 if (!invalidateOnChange) {
                     this.skip();
                 }
                 await map1.get('key1');
                 await map2.remove('key1');
-                const val = await TestUtil.promiseLater(1000, map1.get.bind(map1, 'key1'));
-                expectStats(map1, 0, 2, 1);
-                expect(val).to.be.null;
+                await TestUtil.assertTrueEventually(async () => {
+                    const val = await map1.get('key1');
+                    expectStats(map1, 0, 2, 1);
+                    expect(val).to.be.null;
+                }, 1000);
             });
 
             it('clear clears nearcache', async function () {
