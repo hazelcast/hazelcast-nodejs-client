08fda2ebfd72929b9982168e6d67e7ced376914c
Serkan Özel <serkan.ozel@hazelcast.com>
2022-01-26 13:51:28 +0300

    Update statistics test

--- test/integration/backward_compatible/serial/statistics/StatisticsTest.js
+++ test/integration/backward_compatible/serial/statistics/StatisticsTest.js
@@ -60,128 +60,155 @@ function extractIntStatValue(stats, statName) {
 }
 
 const testFactory = new TestUtil.TestFactory();
+[true, false].forEach((useProps) => {
+    describe('StatisticsTest (default period) ' + (useProps ? 'using statistics config' : 'using metrics config'), function () {
+        let cluster;
+        let client;
+        let map;
 
-describe('StatisticsTest (default period)', function () {
-    let cluster;
-    let client;
-    let map;
-
-    before(async function () {
-        cluster = await testFactory.createClusterForSerialTests();
-        const member = await RC.startMember(cluster.id);
-        client = await testFactory.newHazelcastClientForSerialTests({
-            clusterName: cluster.id,
-            nearCaches: {
-                'nearCachedMap*': {
-                    invalidateOnChange: false
-                }
-            },
-            properties: {
-                'hazelcast.client.statistics.enabled': true
-            },
-            network: {
-                clusterMembers: [`127.0.0.1:${member.port}`]
+        before(async function () {
+            // metrics added in 5.1.0
+            if (!useProps) {
+                TestUtil.markClientVersionAtLeast(this, '5.1.0');
             }
+            cluster = await testFactory.createClusterForSerialTests();
+            const member = await RC.startMember(cluster.id);
+            const config = {
+                clusterName: cluster.id,
+                nearCaches: {
+                    'nearCachedMap*': {
+                        invalidateOnChange: false
+                    }
+                },
+                network: {
+                    clusterMembers: [`127.0.0.1:${member.port}`]
+                }
+            };
+            if (useProps) {
+                config.properties = {
+                    'hazelcast.client.statistics.enabled': true
+                };
+            } else {
+                config.metrics = {
+                    enabled: true
+                };
+            }
+            client = await testFactory.newHazelcastClientForSerialTests(config);
         });
-    });
 
-    after(async function () {
-        await testFactory.shutdownAll();
-    });
+        after(async function () {
+            await testFactory.shutdownAll();
+        });
 
-    beforeEach(async function () {
-        map = await client.getMap('nearCachedMap' + Math.random());
-    });
+        beforeEach(async function () {
+            map = await client.getMap('nearCachedMap' + Math.random());
+        });
 
-    afterEach(async function () {
-        await map.destroy();
-    });
+        afterEach(async function () {
+            await map.destroy();
+        });
 
-    function containsOnce(base, search) {
-        const firstIndex = base.indexOf(search);
-        return firstIndex > -1 && firstIndex === base.lastIndexOf(search);
-    }
+        function containsOnce(base, search) {
+            const firstIndex = base.indexOf(search);
+            return firstIndex > -1 && firstIndex === base.lastIndexOf(search);
+        }
 
-    it('should be enabled via configuration', async function () {
-        await TestUtil.promiseWaitMilliseconds(1000);
-        const stats = await getClientStatisticsFromServer(cluster, client);
-        expect(stats).to.not.null;
-        expect(stats).to.not.equal('');
-    });
+        it('should be enabled via configuration', async function () {
+            await TestUtil.promiseWaitMilliseconds(1000);
+            const stats = await getClientStatisticsFromServer(cluster, client);
+            expect(stats).to.not.null;
+            expect(stats).to.not.equal('');
+        });
 
-    it('should contain statistics content', async function () {
-        await TestUtil.promiseWaitMilliseconds(1000);
-        const stats = await getClientStatisticsFromServer(cluster, client);
-        expect(stats).to.not.be.null;
-        expect(extractStringStatValue(stats, 'clientName')).to.equal(client.getName());
-        expect(extractIntStatValue(stats, 'lastStatisticsCollectionTime')).to.be
-            .within(Date.now() - Statistics.PERIOD_SECONDS_DEFAULT_VALUE * 2000, Date.now());
-        expect(extractBooleanStatValue(stats, 'enterprise')).to.be.false;
-        const expectedClientType = TestUtil.isClientVersionAtLeast('4.0.2') ? 'NJS' : 'NodeJS';
-        expect(extractStringStatValue(stats, 'clientType')).to.equal(expectedClientType);
-        expect(extractStringStatValue(stats, 'clientVersion')).to.equal(BuildInfo.getClientVersion());
-        const connection = TestUtil.getRandomConnection(client);
-        expect(extractIntStatValue(stats, 'clusterConnectionTimestamp')).to.equal(connection.getStartTime());
-        expect(extractStringStatValue(stats, 'clientAddress')).to.equal(connection.getLocalAddress().toString());
-        expect(extractIntStatValue(stats, 'os.processCpuTime')).to.greaterThan(1000);
-        expect(extractFloatStatValue(stats, 'os.systemLoadAverage')).to.be.at.least(0);
-        expect(extractIntStatValue(stats, 'os.totalPhysicalMemorySize')).to.equal(os.totalmem());
-        expect(extractIntStatValue(stats, 'runtime.availableProcessors')).to.equal(os.cpus().length);
-        expect(extractIntStatValue(stats, 'runtime.totalMemory')).to.greaterThan(0);
-        expect(extractIntStatValue(stats, 'runtime.uptime')).to.greaterThan(0);
-        expect(extractIntStatValue(stats, 'runtime.usedMemory')).to.greaterThan(0);
-    });
+        it('should contain statistics content', async function () {
+            await TestUtil.promiseWaitMilliseconds(1000);
+            const stats = await getClientStatisticsFromServer(cluster, client);
+            expect(stats).to.not.be.null;
+            expect(extractStringStatValue(stats, 'clientName')).to.equal(client.getName());
+            expect(extractIntStatValue(stats, 'lastStatisticsCollectionTime')).to.be
+                .within(Date.now() - Statistics.PERIOD_SECONDS_DEFAULT_VALUE * 2000, Date.now());
+            expect(extractBooleanStatValue(stats, 'enterprise')).to.be.false;
+            const expectedClientType = TestUtil.isClientVersionAtLeast('4.0.2') ? 'NJS' : 'NodeJS';
+            expect(extractStringStatValue(stats, 'clientType')).to.equal(expectedClientType);
+            expect(extractStringStatValue(stats, 'clientVersion')).to.equal(BuildInfo.getClientVersion());
+            const connection = TestUtil.getRandomConnection(client);
+            expect(extractIntStatValue(stats, 'clusterConnectionTimestamp')).to.equal(connection.getStartTime());
+            expect(extractStringStatValue(stats, 'clientAddress')).to.equal(connection.getLocalAddress().toString());
+            expect(extractIntStatValue(stats, 'os.processCpuTime')).to.greaterThan(1000);
+            expect(extractFloatStatValue(stats, 'os.systemLoadAverage')).to.be.at.least(0);
+            expect(extractIntStatValue(stats, 'os.totalPhysicalMemorySize')).to.equal(os.totalmem());
+            expect(extractIntStatValue(stats, 'runtime.availableProcessors')).to.equal(os.cpus().length);
+            expect(extractIntStatValue(stats, 'runtime.totalMemory')).to.greaterThan(0);
+            expect(extractIntStatValue(stats, 'runtime.uptime')).to.greaterThan(0);
+            expect(extractIntStatValue(stats, 'runtime.usedMemory')).to.greaterThan(0);
+        });
 
-    it('should contain near cache statistics content', async function () {
-        await map.put('key', 'value');
-        await map.get('key');
-        await map.get('key');
-        await TestUtil.promiseWaitMilliseconds(5000);
-        const stats = await getClientStatisticsFromServer(cluster, client);
-        const nearCacheStats = 'nc.' + map.getName();
-        expect(containsOnce(stats, nearCacheStats + '.hits=1')).to.be.true;
-        expect(containsOnce(stats, nearCacheStats + '.creationTime=')).to.be.true;
-        expect(containsOnce(stats, nearCacheStats + '.misses=1')).to.be.true;
-        expect(containsOnce(stats, nearCacheStats + '.ownedEntryCount=1')).to.be.true;
+        it('should contain near cache statistics content', async function () {
+            await map.put('key', 'value');
+            await map.get('key');
+            await map.get('key');
+            await TestUtil.promiseWaitMilliseconds(5000);
+            const stats = await getClientStatisticsFromServer(cluster, client);
+            const nearCacheStats = 'nc.' + map.getName();
+            expect(containsOnce(stats, nearCacheStats + '.hits=1')).to.be.true;
+            expect(containsOnce(stats, nearCacheStats + '.creationTime=')).to.be.true;
+            expect(containsOnce(stats, nearCacheStats + '.misses=1')).to.be.true;
+            expect(containsOnce(stats, nearCacheStats + '.ownedEntryCount=1')).to.be.true;
+        });
     });
 });
 
-describe('StatisticsTest (non-default period)', function () {
-    let cluster;
-    let client;
+[true, false].forEach((useProps) => {
+    describe('StatisticsTest (non-default period) ' + (useProps ? 'using statistics config' : 'using metrics config'),
+        function () {
+        let cluster;
+        let client;
 
-    before(async function () {
-        cluster = await testFactory.createClusterForSerialTests();
-        const member = await RC.startMember(cluster.id);
-        client = await testFactory.newHazelcastClientForSerialTests({
-            clusterName: cluster.id,
-            properties: {
-                'hazelcast.client.statistics.enabled': true,
-                'hazelcast.client.statistics.period.seconds': 2
-            },
-            network: {
-                clusterMembers: [`127.0.0.1:${member.port}`]
+        before(async function () {
+            // metrics added in 5.1.0
+            if (!useProps) {
+                TestUtil.markClientVersionAtLeast(this, '5.1.0');
             }
+            cluster = await testFactory.createClusterForSerialTests();
+            const member = await RC.startMember(cluster.id);
+            const config = {
+                clusterName: cluster.id,
+                network: {
+                    clusterMembers: [`127.0.0.1:${member.port}`]
+                }
+            };
+            if (useProps) {
+                config.properties = {
+                    'hazelcast.client.statistics.enabled': true,
+                    'hazelcast.client.statistics.period.seconds': 2
+                };
+            } else {
+                config.metrics = {
+                    enabled: true,
+                    collectionFrequencySeconds: 2
+                };
+            }
+            client = await testFactory.newHazelcastClientForSerialTests(config);
         });
-    });
 
-    after(async function () {
-        await testFactory.shutdownAll();
-    });
+        after(async function () {
+            await testFactory.shutdownAll();
+        });
 
-    it('should not change before period', async function () {
-        await TestUtil.promiseWaitMilliseconds(1000);
-        const stats1 = await getClientStatisticsFromServer(cluster, client);
-        const stats2 = await getClientStatisticsFromServer(cluster, client);
-        expect(stats1).to.be.equal(stats2);
-    });
+        it('should not change before period', async function () {
+            await TestUtil.promiseWaitMilliseconds(1000);
+            const stats1 = await getClientStatisticsFromServer(cluster, client);
+            const stats2 = await getClientStatisticsFromServer(cluster, client);
+            expect(stats1).to.be.equal(stats2);
+        });
 
-    it('should change after period', async function () {
-        await TestUtil.promiseWaitMilliseconds(1000);
-        const stats1 = await getClientStatisticsFromServer(cluster, client);
-        await TestUtil.promiseWaitMilliseconds(2000);
-        const stats2 = await getClientStatisticsFromServer(cluster, client);
-        expect(stats1).not.to.be.equal(stats2);
+        it('should change after period', async function () {
+            await TestUtil.promiseWaitMilliseconds(1000);
+            const stats1 = await getClientStatisticsFromServer(cluster, client);
+            await TestUtil.promiseWaitMilliseconds(2000);
+            const stats2 = await getClientStatisticsFromServer(cluster, client);
+            expect(stats1).not.to.be.equal(stats2);
+        });
     });
 });
 
