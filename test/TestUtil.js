a4111bad030e11bd0c7acd2113a3265d636cabe2
Serkan Özel <serkan.ozel@hazelcast.com>
2022-03-03 13:49:48 +0300

    Fix typo

--- test/TestUtil.js
+++ test/TestUtil.js
@@ -152,7 +152,7 @@ exports.getConnections = function(client) {
     if (Object.prototype.hasOwnProperty.call(client, 'connectionRegistry')) {
         return client.connectionRegistry.getConnections();
     } else {
-        return client.getConnectionManager().getConnections();
+        return client.getConnectionManager().getActiveConnections();
     }
 };
 
