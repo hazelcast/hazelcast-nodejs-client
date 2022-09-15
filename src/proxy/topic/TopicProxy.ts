deb1103bb0f5370cf8eb177aa914f4bf54bd0245
fatihozer0 <fatih.ozer@hazelcast.com>
2022-09-15 13:09:50 +0300

    fix some mistakes on Topic Impl

--- src/proxy/topic/TopicProxy.ts
+++ src/proxy/topic/TopicProxy.ts
@@ -3,7 +3,6 @@ import {MessageListener} from '../MessageListener';
 import {UUID} from '../../core';
 import {TopicAddMessageListenerCodec} from '../../codec/TopicAddMessageListenerCodec';
 import {TopicRemoveMessageListenerCodec} from '../../codec/TopicRemoveMessageListenerCodec';
-import {TopicPublishCodec} from '../../codec/TopicPublishCodec';
 import { ListenerMessageCodec} from '../../listener/ListenerMessageCodec';
 import {ClientMessage} from '../../protocol/ClientMessage';
 import {assertNotNull} from '../../util/Util';
@@ -13,8 +12,6 @@ export class TopicProxy<E> extends PartitionSpecificProxy implements ITopic<E> {
 
     publish(message: E): Promise<void> {
         assertNotNull(message);
-        const data = this.toData(message);
-        const request = TopicPublishCodec.encodeRequest(this.name, data);
         return this.encodeInvoke<void>(TopicAddMessageListenerCodec,
                                        TopicAddMessageListenerCodec.decodeResponse);
     }
@@ -22,13 +19,16 @@ export class TopicProxy<E> extends PartitionSpecificProxy implements ITopic<E> {
     addMessageListener(listener: MessageListener<E>): string {
         assertNotNull(listener);
         const message = TopicAddMessageListenerCodec.encodeRequest(this.name, false);
-        const codec = this.createListenerMessageCodec(this.name)
+        const codec = this.createListenerMessageCodec()
         TopicAddMessageListenerCodec.handle(message, () => {});
         // The error message :
-        // TS2345: Argument of type 'MessageListener&lt;E&gt;' is not assignable to parameter
+        // TS2345: Argument of type 'MessageListener<E>' is not assignable to parameter
         // of type 'ClientMessageHandler'.<br/>Types of parameters 'message' and 'message'
-        // are incompatible.<br/>Type 'ClientMessage' is missing the following properties
-        // from type 'Message&lt;E&gt;': messageObject, publisher, publishingTime
+        // are incompatible.Type 'ClientMessage' is missing the following properties
+        // from type 'Message<E>': messageObject, publisher, publishingTime
+        // Other error message:
+        // TS2322: Type 'Promise<string>' is not assignable to type 'string'.
+
         return this.listenerService.registerListener(codec, listener);
     }
 
@@ -42,7 +42,7 @@ export class TopicProxy<E> extends PartitionSpecificProxy implements ITopic<E> {
         return this.listenerService.deregisterListener(listenerId);
     }
 
-    private createListenerMessageCodec(name: string): ListenerMessageCodec {
+    private createListenerMessageCodec(): ListenerMessageCodec {
         return {
             encodeAddRequest(): ClientMessage {
                 return TopicAddMessageListenerCodec.encodeRequest(this.name, this.localOnly);
