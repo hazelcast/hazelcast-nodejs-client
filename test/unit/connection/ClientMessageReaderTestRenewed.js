7e040179e8cef26fd05b8b8237a90ba356ac066f
fatihozer0 <fatih.ozer@hazelcast.com>
2022-11-05 17:35:14 +0300

    add all the test cases of Java

--- test/unit/connection/ClientMessageReaderTestRenewed.js
+++ test/unit/connection/ClientMessageReaderTestRenewed.js
@@ -4,18 +4,18 @@ const { expect } = require('chai');
 
 const { ClientMessageReader } = require('../../../lib/network/Connection');
 const cm = require('../../../lib/protocol/ClientMessage');
-const {ClientMessage} = require("../../../lib/protocol/ClientMessage");
+const {ClientMessage, Frame} = require("../../../lib/protocol/ClientMessage");
 
 describe('ClientMessageReaderTest', function () {
 
     const HEADER_SIZE = cm.SIZE_OF_FRAME_LENGTH_AND_FLAGS;
 
     function createFrameWithRandomBytes(length) {
-        const frame = Buffer.allocUnsafe(length);
+        const buffer = Buffer.allocUnsafe(length);
         for (let i = 0; i < length; i++) {
-            frame.writeInt8(Math.floor(Math.random()), i);
+            buffer.writeInt8(Math.floor(Math.random()), i);
         }
-        return frame;
+        return new Frame(buffer);
     }
 
     function writeToBuffer(message) {
@@ -37,6 +37,119 @@ describe('ClientMessageReaderTest', function () {
         const buffer = writeToBuffer(message);
         reader.append(buffer);
 
-        expect(reader.readFromReader(buffer)).to.be.true;
+        const messageRead = reader.read();
+        expect(messageRead.hasNextFrame()).to.be.true;
+
+        const frameRead = messageRead.nextFrame();
+        expect(frameRead.content).to.deep.equal(frame.content);
+
+        expect(messageRead.hasNextFrame()).to.be.false;
+    });
+
+    it('reads multiple messages', function() {
+        const frame1 = createFrameWithRandomBytes(10);
+        const frame2 = createFrameWithRandomBytes(20);
+        const frame3 = createFrameWithRandomBytes(30);
+
+        const message = ClientMessage.createForEncode(0, 0, 0);
+        message.addFrame(frame1);
+        message.addFrame(frame2);
+        message.addFrame(frame3);
+
+        const buffer = writeToBuffer(message);
+        reader.append(buffer);
+
+        const messageRead = reader.read();
+        expect(messageRead.hasNextFrame()).to.be.true;
+        let frameRead = messageRead.nextFrame();
+        expect(frameRead.content).to.deep.equal(frame1.content);
+
+        expect(messageRead.hasNextFrame()).to.be.true;
+        frameRead = messageRead.nextFrame();
+        expect(frameRead.content).to.deep.equal(frame2.content);
+
+        expect(messageRead.hasNextFrame()).to.be.true;
+        frameRead = messageRead.nextFrame();
+        expect(frameRead.content).to.deep.equal(frame3.content);
+
+        expect(messageRead.hasNextFrame()).to.be.false;
+    });
+
+    it('reads frames in multiple calls', function() {
+        const frame = createFrameWithRandomBytes(1000);
+
+        const message = ClientMessage.createForEncode(0, 0, 0);
+        message.addFrame(frame);
+
+        const buffer = writeToBuffer(message);
+
+        const firstPartition = buffer.slice(0, 750);
+        const secondPartition = buffer.slice(750);
+
+        reader.append(firstPartition);
+        reader.append(secondPartition);
+
+        const messageRead = reader.read();
+        expect(messageRead.hasNextFrame()).to.be.true;
+
+        const frameRead = messageRead.nextFrame();
+        expect(frameRead.content).to.deep.equal(frame.content);
+
+        expect(messageRead.hasNextFrame()).to.be.false;
+    });
+
+    it('reads frames in multiple calls when last piece is small', function() {
+        const frame = createFrameWithRandomBytes(1000);
+
+        const message = ClientMessage.createForEncode(0, 0, 0);
+        message.addFrame(frame);
+
+        const buffer = writeToBuffer(message);
+
+        const firstPartition = buffer.slice(0, 750);
+        const secondPartition = buffer.slice(750, 1002);
+        const thirdPartition = buffer.slice(1002);
+
+        reader.append(firstPartition);
+        reader.append(secondPartition);
+        reader.append(thirdPartition);
+
+        const messageRead = reader.read();
+        expect(messageRead.hasNextFrame()).to.be.true;
+
+        const frameRead = messageRead.nextFrame();
+        expect(frameRead.content).to.deep.equal(frame.content);
+
+        expect(messageRead.hasNextFrame()).to.be.false;
+    });
+
+    it('reads when the Frame length and Flags not received at first', function() {
+        const frame = createFrameWithRandomBytes(100);
+
+        const message = ClientMessage.createForEncode(0, 0, 0);
+        message.addFrame(frame);
+
+        const buffer = writeToBuffer(message);
+        const capacity = buffer.length;
+        let limit = 4;
+
+        const firstPartition = buffer.slice(0, limit);
+
+        reader.append(firstPartition);
+
+        let messageRead = reader.read();
+        expect(messageRead).to.be.null;
+
+        limit = capacity;
+        const secondPartition = buffer.slice(4, limit);
+        reader.append(secondPartition);
+
+        messageRead = reader.read();
+        expect(messageRead.hasNextFrame()).to.be.true;
+
+        const frameRead = messageRead.nextFrame();
+        expect(frameRead.content).to.deep.equal(frame.content);
+
+        expect(messageRead.hasNextFrame()).to.be.false;
     });
 });
