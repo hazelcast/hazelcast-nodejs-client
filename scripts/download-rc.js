183fc8344719dc9117197260de691eab62cb01c3
Jack <jack.green@hazelcast.com>
2025-09-18 13:10:22 +0100

    relocate

--- scripts/download-rc.js
+++ scripts/download-rc.js
@@ -44,7 +44,7 @@ const downloadRC = () => {
     }
 
     function downloadArtifact(repo, artifactId, version, classifier = '') {
-        const filename = classifier ? `hazelcast-${version}-${classifier}.jar` : `hazelcast-${version}.jar`;
+        const filename = classifier ? `${artifactId}-${version}-${classifier}.jar` : `hazelcast-${version}.jar`;
         let artifact = `com.hazelcast:${artifactId}:${version}:jar`;
         if (classifier) {
             artifact += `:${classifier}`;
